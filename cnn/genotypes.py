bb43230d15aa40d6bb6faa9c9f85d7eb44656486
yochaiz <yochaizur@gmail.com>
2018-07-19 19:10:51 +0300

    * put genotypes in comment, in order to check if it is relevant to anything...

--- cnn/genotypes.py
+++ cnn/genotypes.py
@@ -2,16 +2,16 @@ from collections import namedtuple
 
 Genotype = namedtuple('Genotype', 'normal normal_concat reduce reduce_concat')
 
-PRIMITIVES = [
-    'none',
-    'max_pool_3x3',
-    'avg_pool_3x3',
-    'skip_connect',
-    'sep_conv_3x3',
-    'sep_conv_5x5',
-    'dil_conv_3x3',
-    'dil_conv_5x5'
-]
+# PRIMITIVES = [
+#     'none',
+#     'max_pool_3x3',
+#     'avg_pool_3x3',
+#     'skip_connect',
+#     'sep_conv_3x3',
+#     'sep_conv_5x5',
+#     'dil_conv_3x3',
+#     'dil_conv_5x5'
+# ]
 
 NASNet = Genotype(
     normal=[
@@ -71,8 +71,8 @@ AmoebaNet = Genotype(
     reduce_concat=[3, 4, 6]
 )
 
-DARTS = Genotype(
-    normal=[('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('skip_connect', 0), ('sep_conv_3x3', 1), ('skip_connect', 0),
-            ('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('skip_connect', 2)], normal_concat=[2, 3, 4, 5],
-    reduce=[('max_pool_3x3', 0), ('max_pool_3x3', 1), ('skip_connect', 2), ('max_pool_3x3', 0), ('max_pool_3x3', 0),
-            ('skip_connect', 2), ('skip_connect', 2), ('avg_pool_3x3', 0)], reduce_concat=[2, 3, 4, 5])
+# DARTS = Genotype(
+#     normal=[('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('skip_connect', 0), ('sep_conv_3x3', 1), ('skip_connect', 0),
+#             ('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('skip_connect', 2)], normal_concat=[2, 3, 4, 5],
+#     reduce=[('max_pool_3x3', 0), ('max_pool_3x3', 1), ('skip_connect', 2), ('max_pool_3x3', 0), ('max_pool_3x3', 0),
+#             ('skip_connect', 2), ('skip_connect', 2), ('avg_pool_3x3', 0)], reduce_concat=[2, 3, 4, 5])
