ba04ab6516335088b1013ac44c6cbe46dd5348f8
yochaiz <yochaizur@gmail.com>
2018-07-19 19:13:05 +0300

    * irrelevant file

--- cnn/genotypes.py
+++ cnn/genotypes.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-Genotype = namedtuple('Genotype', 'normal normal_concat reduce reduce_concat')
+# Genotype = namedtuple('Genotype', 'normal normal_concat reduce reduce_concat')
 
 # PRIMITIVES = [
 #     'none',
