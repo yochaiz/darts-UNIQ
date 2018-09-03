76ebb1b7a3f38d016398c35bc7766f6089dba8dc
brianchmiel <pancho2787>
2018-09-03 13:06:17 +0300

    * init optimizer in every step

--- cnn/architect.py
+++ cnn/architect.py
@@ -15,11 +15,15 @@ class Architect(object):
     def __init__(self, model, args):
         self.network_momentum = args.momentum
         self.network_weight_decay = args.weight_decay
+        self.lr = args.arch_learning_rate
+        self.weight_decay = args.arch_weight_decay
+
         self.model = model
+
         # self.optimizer = Adam(self.model.arch_parameters(), lr=args.arch_learning_rate,
         #                       betas=(0.5, 0.999), weight_decay=args.arch_weight_decay)
-        self.optimizer = SGD(self.model.arch_parameters(), lr=args.arch_learning_rate, momentum=args.momentum,
-                             weight_decay=args.arch_weight_decay)
+        # self.optimizer = SGD(self.model.arch_parameters(), lr=args.arch_learning_rate, momentum=args.momentum,
+        #                      weight_decay=args.arch_weight_decay)
 
     def _compute_unrolled_model(self, input, target, eta, network_optimizer):
         loss = self.model._loss(input, target)
@@ -34,25 +38,35 @@ class Architect(object):
         return model_unrolled
 
     def step(self, input_train, target_train, input_valid, target_valid, eta, network_optimizer, unrolled):
+        arch_parameters = self.model.arch_parameters()
+        # init optimizer for current arch_parameters
+
+        self.optimizer = SGD(arch_parameters, lr=self.lr, momentum=self.network_momentum)
+        # , weight_decay=self.weight_decay)
         self.optimizer.zero_grad()
         if unrolled:
-            self._backward_step_unrolled(input_train, target_train, input_valid, target_valid, eta, network_optimizer)
+            loss = self._backward_step_unrolled(input_train, target_train, input_valid, target_valid, eta,
+                                                network_optimizer)
         else:
-            self._backward_step(input_valid, target_valid)
+            loss = self._backward_step(input_valid, target_valid)
+        # print('grads:{}'.format(self.model.block1.alphas.grad))
+        grad_norm = clip_grad_norm(arch_parameters, 10.0)
 
-        grad_norm = clip_grad_norm(self.model.arch_parameters(), 10.)
+        # print('grad_norm:{}'.format(grad_norm))
+        # print('alphas:{}'.format(self.model.block1.alphas))
+        # for p in self.optimizer.param_groups:
+        #     p['params'][0].grad.data.clamp_(min=0)
         self.optimizer.step()
-        return grad_norm
+
+        return grad_norm, loss
 
     def _backward_step(self, input_valid, target_valid):
         loss = self.model._loss(input_valid, target_valid)
-        for v in self.model.arch_parameters():
-            if v.grad is not None:
-                v.grad.data.zero_()
+        # for v in self.model.arch_parameters():
+        #     if v.grad is not None:
+        #         v.grad.data.zero_()
         loss.backward(retain_graph=True)
-
-
-
+        return loss
 
     def _backward_step_unrolled(self, input_train, target_train, input_valid, target_valid, eta, network_optimizer):
         model_unrolled = self._compute_unrolled_model(input_train, target_train, eta, network_optimizer)
@@ -72,6 +86,8 @@ class Architect(object):
                 v.grad = Variable(g.data)
             else:
                 v.grad.data.copy_(g.data)
+
+        return loss
 
     def _construct_model_from_theta(self, theta):
         model_clone = Network(self.model._C, self.model._num_classes, self.model._layers, self.model._criterion).cuda()
