b23020d8904aad0d08c4e5d6789f20c8d2274973
brianchmiel <pancho2787>
2018-09-05 10:30:33 +0300

    * we train weights & alphas separately

--- cnn/optimize.py
+++ cnn/optimize.py
@@ -12,12 +12,10 @@ from cnn.utils import initTrainLogger, logDominantQuantizedOp, save_checkpoint
 from cnn.architect import Architect
 
 
-def train(train_queue, search_queue, args, model, architect, crit, optimizer, lr, logger):
-    weights_loss_container = AvgrageMeter()
-    arch_loss_container = AvgrageMeter()
+def trainWeights(train_queue, search_queue, args, model, architect, crit, optimizer, lr, logger):
+    loss_container = AvgrageMeter()
     top1 = AvgrageMeter()
     top5 = AvgrageMeter()
-    grad = AvgrageMeter()
 
     model.train()
 
@@ -30,16 +28,6 @@ def train(train_queue, search_queue, args, model, architect, crit, optimizer, lr
         input = Variable(input, requires_grad=False).cuda()
         target = Variable(target, requires_grad=False).cuda(async=True)
 
-        # get a random minibatch from the search queue with replacement
-        if (len(search_queue) > 0) and (len(model.arch_parameters()) > 0):
-            input_search, target_search = next(iter(search_queue))
-            input_search = Variable(input_search, requires_grad=False).cuda()
-            target_search = Variable(target_search, requires_grad=False).cuda(async=True)
-
-            arch_grad_norm, arch_loss = architect.step(input, target, input_search, target_search, lr,
-                                                       optimizer, unrolled=args.unrolled)
-            grad.update(arch_grad_norm)
-
         # choose optimal alpha per layer
         bopsRatio = model.trainMode()
         # optimize model weights
@@ -51,28 +39,49 @@ def train(train_queue, search_queue, args, model, architect, crit, optimizer, lr
         # print('w grads:{}'.format(model.block1.alphas.grad))
         optimizer.step()
 
-        # normalize alphas
-        # for alphas in model.arch_parameters():
-        #     minNorm = abs(alphas).min()
-        #     alphas.data = tensor((alphas / minNorm).cuda(), requires_grad=True)
-
         prec1, prec5 = accuracy(logits, target, topk=(1, 5))
-        weights_loss_container.update(loss.item(), n)
-        arch_loss_container.update(arch_loss.item(), len(search_queue))
+        loss_container.update(loss.item(), n)
         top1.update(prec1.item(), n)
         top5.update(prec5.item(), n)
 
         endTime = time()
         if step % args.report_freq == 0:
             logger.info(
-                'train [{}/{}] weight_loss:[{:.5f}] Accuracy:[{:.3f}] arch_loss:[{:.5f}] BopsRatio:[{:.3f}] time:[{:.5f}]'
-                    .format(step, nBatches, weights_loss_container.avg, top1.avg, arch_loss_container.avg,
-                            bopsRatio, endTime - startTime))
+                'train [{}/{}] weight_loss:[{:.5f}] Accuracy:[{:.3f}] BopsRatio:[{:.3f}] time:[{:.5f}]'
+                    .format(step, nBatches, loss_container.avg, top1.avg, bopsRatio, endTime - startTime))
 
-    return top1.avg, weights_loss_container.avg, arch_loss_container.avg
+    return top1.avg, loss_container.avg
 
 
-def infer(valid_queue, args, model, crit, logger):
+def trainAlphas(search_queue, model, architect, logger):
+    loss_container = AvgrageMeter()
+
+    model.train()
+    model.trainMode()
+
+    nBatches = len(search_queue)
+
+    for step, (input, target) in enumerate(search_queue):
+        startTime = time()
+        n = input.size(0)
+
+        input = Variable(input, requires_grad=False).cuda()
+        target = Variable(target, requires_grad=False).cuda(async=True)
+
+        loss = architect.step(model, input, target)
+
+        # log dominant QuantizedOp in each layer
+        logDominantQuantizedOp(model, k=3, logger=logger)
+        loss_container.update(loss, n)
+
+        endTime = time()
+        logger.info('train [{}/{}] arch_loss:[{:.5f}] time:[{:.5f}]'
+                    .format(step, nBatches, loss_container.avg, endTime - startTime))
+
+    return loss_container.avg
+
+
+def infer(valid_queue, model, crit, logger):
     objs = AvgrageMeter()
     top1 = AvgrageMeter()
     top5 = AvgrageMeter()
@@ -102,15 +111,13 @@ def infer(valid_queue, args, model, crit, logger):
 
             endTime = time()
 
-            if step % args.report_freq == 0:
-                logger.info(
-                    'validation [{}/{}] Loss:[{:.5f}] Accuracy:[{:.3f}] BopsRatio:[{:.3f}] time:[{:.5f}]'
+            logger.info('validation [{}/{}] Loss:[{:.5f}] Accuracy:[{:.3f}] BopsRatio:[{:.3f}] time:[{:.5f}]'
                         .format(step, nBatches, objs.avg, top1.avg, bopsRatio, endTime - startTime))
 
     return top1.avg, objs.avg
 
 
-def optimize(args, model, logger):
+def optimize(args, model, modelReplicator, logger):
     trainFolderPath = '{}/{}'.format(args.save, args.trainFolder)
 
     optimizer = SGD(model.parameters(), args.learning_rate, momentum=args.momentum, weight_decay=args.weight_decay)
@@ -139,7 +146,7 @@ def optimize(args, model, logger):
     logger.info('epochsSwitchStage:{}'.format(epochsSwitchStage))
 
     scheduler = CosineAnnealingLR(optimizer, float(nEpochs), eta_min=args.learning_rate_min)
-    architect = Architect(model, args)
+    architect = Architect(modelReplicator, args)
 
     best_prec1 = 0.0
 
@@ -156,12 +163,12 @@ def optimize(args, model, logger):
 
         # training
         print('========== Epoch:[{}] =============='.format(epoch))
-        train_acc, train_loss, arch_loss = train(train_queue, search_queue, args, model, architect, cross_entropy,
-                                                 optimizer, lr, trainLogger)
+        train_acc, train_loss = trainWeights(train_queue, search_queue, args, model, architect, cross_entropy,
+                                             optimizer, lr, trainLogger)
 
         # log accuracy, loss, etc.
-        message = 'Epoch:[{}] , training accuracy:[{:.3f}] , training loss:[{:.3f}] , arch loss:[{:.3f}] , optimizer_lr:[{:.5f}], scheduler_lr:[{:.5f}]' \
-            .format(epoch, train_acc, train_loss, arch_loss, optimizer.defaults['lr'], lr)
+        message = 'Epoch:[{}] , training accuracy:[{:.3f}] , training loss:[{:.3f}] , optimizer_lr:[{:.5f}], scheduler_lr:[{:.5f}]' \
+            .format(epoch, train_acc, train_loss, optimizer.defaults['lr'], lr)
         logger.info(message)
         trainLogger.info(message)
 
@@ -174,7 +181,7 @@ def optimize(args, model, logger):
         # switch stage, i.e. freeze one more layer
         if (epoch in epochsSwitchStage) or (epoch == nEpochs):
             # validation
-            valid_acc, valid_loss = infer(valid_queue, args, model, cross_entropy, trainLogger)
+            valid_acc, valid_loss = infer(valid_queue, model, cross_entropy, trainLogger)
             message = 'Epoch:[{}] , validation accuracy:[{:.3f}] , validation loss:[{:.3f}]'.format(epoch, valid_acc,
                                                                                                     valid_loss)
             logger.info(message)
@@ -191,3 +198,97 @@ def optimize(args, model, logger):
             optimizer = SGD(model.parameters(), scheduler.get_lr()[0],
                             momentum=args.momentum, weight_decay=args.weight_decay)
             scheduler = CosineAnnealingLR(optimizer, float(nEpochs), eta_min=args.learning_rate_min)
+
+    # turn on alphas
+    model.turnOnAlphas()
+    # init scheduler
+    nEpochs = model.nLayers()
+    scheduler = CosineAnnealingLR(optimizer, float(nEpochs), eta_min=args.learning_rate_min)
+    # train alphas
+    for epoch in range(epoch + 1, epoch + nEpochs + 1):
+        trainLogger = initTrainLogger(str(epoch), trainFolderPath, args.propagate)
+
+        scheduler.step()
+        lr = scheduler.get_lr()[0]
+
+        trainLogger.info('optimizer_lr:[{:.5f}], scheduler_lr:[{:.5f}]'.format(optimizer.defaults['lr'], lr))
+        print('========== Epoch:[{}] =============='.format(epoch))
+        arch_loss = trainAlphas(search_queue, model, architect, trainLogger)
+
+        # log accuracy, loss, etc.
+        message = 'Epoch:[{}] , arch loss:[{:.3f}] , optimizer_lr:[{:.5f}], scheduler_lr:[{:.5f}]' \
+            .format(epoch, arch_loss, optimizer.defaults['lr'], lr)
+        logger.info(message)
+        trainLogger.info(message)
+
+        # validation
+        valid_acc, valid_loss = infer(valid_queue, args, model, cross_entropy, trainLogger)
+        message = 'Epoch:[{}] , validation accuracy:[{:.3f}] , validation loss:[{:.3f}]' \
+            .format(epoch, valid_acc, valid_loss)
+
+        logger.info(message)
+        trainLogger.info(message)
+
+        # save model checkpoint
+        is_best = valid_acc > best_prec1
+        best_prec1 = max(valid_acc, best_prec1)
+        save_checkpoint(trainFolderPath, model, epoch, best_prec1, is_best)
+
+# def train(train_queue, search_queue, args, model, architect, crit, optimizer, lr, logger):
+#     weights_loss_container = AvgrageMeter()
+#     arch_loss_container = AvgrageMeter()
+#     top1 = AvgrageMeter()
+#     top5 = AvgrageMeter()
+#     grad = AvgrageMeter()
+#
+#     model.train()
+#
+#     nBatches = len(train_queue)
+#
+#     for step, (input, target) in enumerate(train_queue):
+#         startTime = time()
+#         n = input.size(0)
+#
+#         input = Variable(input, requires_grad=False).cuda()
+#         target = Variable(target, requires_grad=False).cuda(async=True)
+#
+#         # get a random minibatch from the search queue with replacement
+#         if (len(search_queue) > 0) and (len(model.arch_parameters()) > 0):
+#             input_search, target_search = next(iter(search_queue))
+#             input_search = Variable(input_search, requires_grad=False).cuda()
+#             target_search = Variable(target_search, requires_grad=False).cuda(async=True)
+#
+#             arch_grad_norm, arch_loss = architect.step(input, target, input_search, target_search, lr,
+#                                                        optimizer, unrolled=args.unrolled)
+#             grad.update(arch_grad_norm)
+#
+#         # choose optimal alpha per layer
+#         bopsRatio = model.trainMode()
+#         # optimize model weights
+#         optimizer.zero_grad()
+#         logits = model(input)
+#         loss = crit(logits, target)
+#         loss.backward()
+#         clip_grad_norm(model.parameters(), args.grad_clip)
+#         # print('w grads:{}'.format(model.block1.alphas.grad))
+#         optimizer.step()
+#
+#         # normalize alphas
+#         # for alphas in model.arch_parameters():
+#         #     minNorm = abs(alphas).min()
+#         #     alphas.data = tensor((alphas / minNorm).cuda(), requires_grad=True)
+#
+#         prec1, prec5 = accuracy(logits, target, topk=(1, 5))
+#         weights_loss_container.update(loss.item(), n)
+#         arch_loss_container.update(arch_loss.item(), len(search_queue))
+#         top1.update(prec1.item(), n)
+#         top5.update(prec5.item(), n)
+#
+#         endTime = time()
+#         if step % args.report_freq == 0:
+#             logger.info(
+#                 'train [{}/{}] weight_loss:[{:.5f}] Accuracy:[{:.3f}] arch_loss:[{:.5f}] BopsRatio:[{:.3f}] time:[{:.5f}]'
+#                     .format(step, nBatches, weights_loss_container.avg, top1.avg, arch_loss_container.avg,
+#                             bopsRatio, endTime - startTime))
+#
+#     return top1.avg, weights_loss_container.avg, arch_loss_container.avg
