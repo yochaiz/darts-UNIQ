3e93f03ced28fcc3ac9508ef9ca73c097272d331
brianchmiel <pancho2787>
2018-09-12 12:40:54 +0300

    * new ModifiedResNet model class

--- cnn/models/__init__.py
+++ cnn/models/__init__.py
@@ -2,3 +2,4 @@ from .BaseNet import BaseNet
 from .TinyNet import TinyNet as tinynet
 from .ResNet import ResNet as resnet
 from .ThinResNet import ThinResNet as thin_resnet
+from .ModifiedResNet import ModifiedResNet as mod_resnet
