bbd1872832b5624433215eca17f6f52163b06ba8
brianchmiel <pancho2787>
2018-09-14 10:31:19 +0300

    * replace self.model with model

--- cnn/trainRegime/minAlphaSamplesLoss.py
+++ cnn/trainRegime/minAlphaSamplesLoss.py
@@ -1,8 +1,9 @@
 from .regime import TrainRegime, infer
 from torch.autograd.variable import Variable
 from time import time
-from cnn.utils import  logDominantQuantizedOp, AvgrageMeter, save_checkpoint, initTrainLogger
+from cnn.utils import logDominantQuantizedOp, AvgrageMeter, save_checkpoint, initTrainLogger
 from numpy import argmin
+
 
 class MinimalAlphaSamplesLoss(TrainRegime):
     def __init__(self, args, model, modelClass, logger):
@@ -10,13 +11,13 @@ class MinimalAlphaSamplesLoss(TrainRegime):
 
         self.nSamplesPerAlpha = model.nSamplesPerAlpha
 
-
-    def trainSamplesAlphas(self,loggers):
+    def trainSamplesAlphas(self, loggers):
         # loss_container = AvgrageMeter()
 
         trainLogger = loggers.get('train')
+        model = self.model
 
-        self.model.train()
+        model.train()
 
         nBatches = len(self.search_queue)
         allAlphaAvgLoss = []
@@ -27,41 +28,40 @@ class MinimalAlphaSamplesLoss(TrainRegime):
             input = Variable(input, requires_grad=False).cuda()
             target = Variable(target, requires_grad=False).cuda(async=True)
 
-            self.model.trainMode()
+            model.trainMode()
             currAllAlphaAvgLoss = self.alphaAvgLoss(input, target)
 
             if not allAlphaAvgLoss:
-                allAlphaAvgLoss = currAllAlphaAvgLoss #for first iteration
+                allAlphaAvgLoss = currAllAlphaAvgLoss  # for first iteration
             else:
-                for j, layer in enumerate(self.model.layersList):
-                    allAlphaAvgLoss[j] =  [sum(x) for x in zip(currAllAlphaAvgLoss[j], allAlphaAvgLoss[j])]
-
+                for j, layer in enumerate(model.layersList):
+                    allAlphaAvgLoss[j] = [sum(x) for x in zip(currAllAlphaAvgLoss[j], allAlphaAvgLoss[j])]
 
             # add alphas data to statistics
             # self.model.stats.addBatchData(self.model, self.epoch, step)
 
             # save alphas to csv
-            self.model.save_alphas_to_csv(data=[self.epoch, step])
+            model.save_alphas_to_csv(data=[self.epoch, step])
 
             endTime = time()
 
             if trainLogger:
                 trainLogger.info('train [{}/{}]  time:[{:.5f}]'
-                                 .format(step, nBatches,  endTime - startTime))
+                                 .format(step, nBatches, endTime - startTime))
 
             # loss_container.update(loss, input.size(0))
 
-        for j, layer in enumerate(self.model.layersList):
+        for j, layer in enumerate(model.layersList):
             # choose the alpha with minimum avg loss
             layer.curr_alpha_idx = argmin(allAlphaAvgLoss[j])
-            #make curr_alpha_idx to be the maximum alpha so we can use regular eval mode
-            self.model.layersList[j].alphas[layer.curr_alpha_idx] = max(layer.alphas) + 1
+            # make curr_alpha_idx to be the maximum alpha so we can use regular eval mode
+            layer.alphas[layer.curr_alpha_idx] = max(layer.alphas) + 1
 
         # count current optimal model bops
-        bopsRatio = self.model.evalMode()
+        bopsRatio = model.evalMode()
 
         # log dominant QuantizedOp in each layer
-        logDominantQuantizedOp(self.model, k=3, logger=trainLogger)
+        logDominantQuantizedOp(model, k=3, logger=trainLogger)
 
         # # log accuracy, loss, etc.
         message = ' OptBopsRatio:[{:.3f}] ]' \
@@ -99,39 +99,26 @@ class MinimalAlphaSamplesLoss(TrainRegime):
                 # add alpha loss variance to statistics
                 self.model.stats.containers[self.model.stats.alphaLossVarianceKey][j][i].append(lossVariance.item())
 
-            #save all alpha avg loss
+            # save all alpha avg loss
             allAlphaAvgLoss.append(alphaAvgLoss)
             # turn in coin toss for this layer
             layer.alphas.requires_grad = True
 
         return allAlphaAvgLoss
 
-
-
-
     def train(self):
-            # turn on alphas
-            self.model.turnOnAlphas()
+        # turn on alphas
+        self.model.turnOnAlphas()
 
-            trainLogger = initTrainLogger(str(self.epoch), self.trainFolderPath, self.args.propagate)
-            # set loggers dictionary
-            loggersDict = dict(train=trainLogger, main=self.logger)
-            # train alphas
-            self.trainSamplesAlphas(loggersDict)
+        trainLogger = initTrainLogger(str(self.epoch), self.trainFolderPath, self.args.propagate)
+        # set loggers dictionary
+        loggersDict = dict(train=trainLogger, main=self.logger)
+        # train alphas
+        self.trainSamplesAlphas(loggersDict)
 
-            # validation on current optimal model
-            valid_acc = infer(self.valid_queue, self.model, self.model.evalMode, self.cross_entropy, self.epoch, loggersDict)
+        # validation on current optimal model
+        valid_acc = infer(self.valid_queue, self.model, self.model.evalMode, self.cross_entropy, self.epoch,
+                          loggersDict)
 
-            # save model checkpoint
-            save_checkpoint(self.trainFolderPath, self.model, self.epoch, valid_acc, True)
-
-
-
-
-
-
-
-
-
-
-
+        # save model checkpoint
+        save_checkpoint(self.trainFolderPath, self.model, self.epoch, valid_acc, True)
