e5290b9ed59541e1a416d2ac9194322597fcba7f
brianchmiel <pancho2787>
2018-09-14 17:30:41 +0300

    * TinyNey pre-trained model for specific bitwidths

---
+++ cnn/pre_trained/tinynet_[2,8]/code/UNIQ/data.py
@@ -0,0 +1,23 @@
+from os.path import join
+import torchvision.datasets as datasets
+
+__DATASETS_DEFAULT_PATH = '/media/ssd/Datasets/'
+
+
+def get_dataset(name, train, transform,
+                target_transform=None, download=True, datasets_path=__DATASETS_DEFAULT_PATH):
+    root = datasets_path  # '/mnt/ssd/ImageNet/ILSVRC/Data/CLS-LOC' #os.path.join(datasets_path, name)
+
+    if name == 'cifar10':
+        cifar_ = datasets.CIFAR10(root=root, train=train, transform=transform, target_transform=target_transform,
+                                  download=download)
+        return cifar_
+
+    elif name == 'imagenet':
+        if train:
+            root = join(root, 'train')
+        else:
+            root = join(root, 'val')
+        return datasets.ImageFolder(root=root,
+                                    transform=transform,
+                                    target_transform=target_transform)
