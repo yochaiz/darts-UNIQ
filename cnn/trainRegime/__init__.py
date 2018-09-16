4a8250cc4924c5ac01844b0397707f2749615480
brianchmiel <pancho2787>
2018-09-16 13:40:39 +0300

    * new short names

--- cnn/trainRegime/__init__.py
+++ cnn/trainRegime/__init__.py
@@ -1,4 +1,4 @@
 from .regime import TrainRegime
-from .minAlphaSamplesLoss import MinimalAlphaSamplesLoss
-from .weightsOnceAlphasOnce import WeightsOnceAlphasOnce
-from .alphasOnly import AlphasOnly
+from .minAlphaSamplesLoss import MinimalAlphaSamplesLoss as min_loss
+from .alphasWeightsLoop import AlphasWeightsLoop as alphas_weights_loop
+from .alphasOnly import AlphasOnly as alphas_only
