93da0cd6c45c5cd647be73f5d6add12f532c134e
brianchmiel <pancho2787>
2018-09-17 10:43:59 +0300

    * uniform models results

--- cnn/uniform/resnet_[8]/code/cnn/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .BaseNet import BaseNet
-from .TinyNet import TinyNet as tinynet
-from .ResNet import ResNet as resnet
-from .ThinResNet import ThinResNet as thin_resnet
-from .ModifiedResNet import ModifiedResNet as mod_resnet
