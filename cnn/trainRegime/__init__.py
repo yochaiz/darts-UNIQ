90e47ce0cd63bf6c043254cf30d4674bd755607f
brianchmiel <pancho2787>
2018-09-21 12:49:33 +0300

    * renamed AlphasOnly with capital 1st letter * added RandomSearch class

--- cnn/trainRegime/__init__.py
+++ cnn/trainRegime/__init__.py
@@ -1,4 +1,5 @@
 from .regime import TrainRegime
 from .minAlphaSamplesLoss import MinimalAlphaSamplesLoss as min_loss
 from .alphasWeightsLoop import AlphasWeightsLoop as alphas_weights_loop
-from .alphasOnly import AlphasOnly as alphas_only
+from .AlphasOnly import AlphasOnly as alphas_only
+from .RandomSearch import RandomSearch as random_search
