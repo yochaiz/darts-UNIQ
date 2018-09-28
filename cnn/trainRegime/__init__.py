b7f993a01da641040f02a3229919a1b235bc8dd8
brianchmiel <pancho2787>
2018-09-28 12:37:41 +0300

    * use TrainRegime as optimal model regime, just train weights, without training alphas

--- cnn/trainRegime/__init__.py
+++ cnn/trainRegime/__init__.py
@@ -1,4 +1,4 @@
-from .regime import TrainRegime
+from .regime import TrainRegime as optimal_model
 from .minAlphaSamplesLoss import MinimalAlphaSamplesLoss as min_loss
 from .alphasWeightsLoop import AlphasWeightsLoop as alphas_weights_loop
 from .AlphasOnly import AlphasOnly as alphas_only
