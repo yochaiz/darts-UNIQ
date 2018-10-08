65b6ddd611628c47288f285d3c4a1bba82ff3f4a
brianchmiel <pancho2787>
2018-10-08 17:17:41 +0300

    * moved Block abstarct class to separate file

---
+++ cnn/block.py
@@ -0,0 +1,36 @@
+from torch.nn.modules.module import Module
+from abc import abstractmethod
+
+
+class Block(Module):
+    @abstractmethod
+    def getBops(self, input_bitwidth):
+        raise NotImplementedError('subclasses must override getBops()!')
+
+    @abstractmethod
+    def getCurrentOutputBitwidth(self):
+        raise NotImplementedError('subclasses must override getCurrentOutputBitwidth()!')
+
+    @abstractmethod
+    def getOutputBitwidthList(self):
+        raise NotImplementedError('subclasses must override getOutputBitwidthList()!')
+
+    @abstractmethod
+    def chooseRandomPath(self):
+        raise NotImplementedError('subclasses must override chooseRandomPath()!')
+
+    @abstractmethod
+    def choosePathByAlphas(self):
+        raise NotImplementedError('subclasses must override choosePathByAlphas()!')
+
+    @abstractmethod
+    def evalMode(self):
+        raise NotImplementedError('subclasses must override evalMode()!')
+
+    @abstractmethod
+    def numOfOps(self):
+        raise NotImplementedError('subclasses must override numOfOps()!')
+
+    @abstractmethod
+    def outputLayer(self):
+        raise NotImplementedError('subclasses must override outputLayer()!')
