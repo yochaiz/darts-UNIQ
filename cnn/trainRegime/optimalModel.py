aa3df1cb501b9499aed3da69d8a8098edba5def3
brianchmiel <pancho2787>
2018-10-08 12:10:29 +0300

    * modified args.init_weights_train to allow initial weights training, which is the main target of OptimalModel regime

--- cnn/trainRegime/optimalModel.py
+++ cnn/trainRegime/optimalModel.py
@@ -1,8 +1,12 @@
+from torch.nn.parallel.data_parallel import DataParallel
+
 from .regime import TrainRegime
 
 
 class OptimalModel(TrainRegime):
     def __init__(self, args, model, modelClass, logger):
+        args.init_weights_train = True
+        model = DataParallel(model, args.gpu)
         super(OptimalModel, self).__init__(args, model, modelClass, logger)
 
     def train(self):
