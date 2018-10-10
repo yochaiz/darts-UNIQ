ffcf668cd98b098e48c16ebd9f2e1df473700cf3
brianchmiel <pancho2787>
2018-10-10 19:27:41 +0300

    * no more DataParallel

--- cnn/trainRegime/optimalModel.py
+++ cnn/trainRegime/optimalModel.py
@@ -1,12 +1,9 @@
-from torch.nn.parallel.data_parallel import DataParallel
-
 from .regime import TrainRegime
 
 
 class OptimalModel(TrainRegime):
     def __init__(self, args, model, modelClass, logger):
         args.init_weights_train = True
-        model = DataParallel(model, args.gpu)
         super(OptimalModel, self).__init__(args, model, modelClass, logger)
 
     def train(self):
