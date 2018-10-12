b0f017c98eb839ce0937b6ca934cdc6fc7fb81e2
brianchmiel <pancho2787>
2018-10-12 12:19:59 +0300

    * added cifar-100 dataset

--- UNIQ/data.py
+++ UNIQ/data.py
@@ -4,13 +4,15 @@ import torchvision.datasets as datasets
 __DATASETS_DEFAULT_PATH = '/media/ssd/Datasets/'
 
 
-def get_dataset(name, train, transform,
-                target_transform=None, download=True, datasets_path=__DATASETS_DEFAULT_PATH):
+def get_dataset(name, train, transform, target_transform=None, download=True, datasets_path=__DATASETS_DEFAULT_PATH):
     root = datasets_path  # '/mnt/ssd/ImageNet/ILSVRC/Data/CLS-LOC' #os.path.join(datasets_path, name)
 
     if name == 'cifar10':
-        cifar_ = datasets.CIFAR10(root=root, train=train, transform=transform, target_transform=target_transform,
-                                  download=download)
+        cifar_ = datasets.CIFAR10(root=root, train=train, transform=transform, target_transform=target_transform, download=download)
+        return cifar_
+
+    elif name == 'cifar100':
+        cifar_ = datasets.CIFAR100(root=root, train=train, transform=transform, target_transform=target_transform, download=download)
         return cifar_
 
     elif name == 'imagenet':
@@ -18,6 +20,5 @@ def get_dataset(name, train, transform,
             root = join(root, 'train')
         else:
             root = join(root, 'val')
-        return datasets.ImageFolder(root=root,
-                                    transform=transform,
-                                    target_transform=target_transform)
+
+        return datasets.ImageFolder(root=root, transform=transform, target_transform=target_transform)
