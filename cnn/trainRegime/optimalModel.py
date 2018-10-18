62d7527303a589def1ab068901d52493eb7b991b
brianchmiel <pancho2787>
2018-10-18 13:43:12 +0300

    * make sure model is quantized * update statistics in current model checkpoint, i.e. last checkpoint * update statistics in optimal model checkpoint

--- cnn/trainRegime/optimalModel.py
+++ cnn/trainRegime/optimalModel.py
@@ -1,10 +1,27 @@
-from .regime import TrainRegime
+from torch import save as saveModel
+
+from .regime import TrainRegime, save_checkpoint
 
 
 class OptimalModel(TrainRegime):
-    def __init__(self, args, model, modelClass, logger):
+    def __init__(self, args, logger):
         args.init_weights_train = True
-        super(OptimalModel, self).__init__(args, model, modelClass, logger)
+        super(OptimalModel, self).__init__(args, logger)
 
     def train(self):
-        pass
+        # make sure model is quantized
+        for layer in self.model.layersList:
+            assert (layer.quantized is True)
+            assert (layer.added_noise is False)
+        # update statistics in current model, i.e. last checkpoint
+        self.model.calcStatistics()
+        # save model checkpoint
+        checkpoint, (lastPath, _) = save_checkpoint(self.trainFolderPath, self.model, self.args, self.epoch, self.args.best_prec1, is_best=False)
+        checkpoint['updated_statistics'] = True
+        saveModel(checkpoint, lastPath)
+        self.logger.addInfoToDataTable('Updated statistics in last checkpoint: [{}]'.format(checkpoint['updated_statistics']))
+
+        # update statistics in optimal model
+        optCheckpoint, optPath = self.optimalModelCheckpoint
+        statisticsWereUpdated = self.model.updateCheckpointStatistics(optCheckpoint, optPath)
+        self.logger.addInfoToDataTable('Updated statistics in optimal checkpoint: [{}]'.format(statisticsWereUpdated))
