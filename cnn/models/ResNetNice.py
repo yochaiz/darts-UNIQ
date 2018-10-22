ab3cfe0de48d711e60ca9d9becf3f4efeeb9f095
Evgenii Zheltonozhskii <zheltonozhskiy@gmail.com>
2018-10-22 12:39:07 +0300

    Add NICE ResNet

---
+++ cnn/models/ResNetNice.py
@@ -0,0 +1,372 @@
+from collections import OrderedDict
+
+from torch.nn import AvgPool2d, Linear, ModuleList
+
+from cnn.MixedFilter import MixedConv, MixedConvWithReLU, Block
+from cnn.MixedLayer import MixedLayer
+from cnn.models import BaseNet
+
+
+# init layers creation functions
+def createMixedConvWithReLU(bitwidths, in_planes, kernel_size, stride, input_size, prevLayer):
+    def f():
+        return MixedConvWithReLU(bitwidths, in_planes, 1, kernel_size, stride, input_size, prevLayer)
+
+    return f
+
+
+def createMixedConv(bitwidths, in_planes, kernel_size, stride, input_size, prevLayer):
+    def f():
+        return MixedConv(bitwidths, in_planes, 1, kernel_size, stride, input_size, prevLayer)
+
+    return f
+
+
+class BasicBlock(Block):
+    def __init__(self, bitwidths, in_planes, out_planes, kernel_size, stride, input_size, prevLayer):
+        super(BasicBlock, self).__init__()
+
+        stride1 = stride if in_planes == out_planes else (stride + 1)
+
+        self.block1 = MixedLayer(out_planes,
+                                 createMixedConvWithReLU(bitwidths[0] if isinstance(bitwidths[0], list) else bitwidths,
+                                                         in_planes, kernel_size,
+                                                         stride1, input_size[0], prevLayer), useResidual=False)
+
+        bitwidthIdx = 1
+        self.downsample = None
+        if in_planes != out_planes:
+            downsampleBitwidth = [(b, None) for b, _ in (bitwidths[1] if isinstance(bitwidths[0], list) else bitwidths)]
+            self.downsample = MixedLayer(out_planes,
+                                         createMixedConv(downsampleBitwidth, in_planes, [1], stride1, input_size[0],
+                                                         prevLayer))
+            bitwidthIdx += 1
+
+        self.block2 = MixedLayer(out_planes,
+                                 createMixedConvWithReLU(
+                                     bitwidths[bitwidthIdx] if isinstance(bitwidths[0], list) else bitwidths,
+                                     out_planes,
+                                     kernel_size, stride, input_size[-1], prevLayer=self.block1), useResidual=True)
+
+    def forward(self, x):
+        residual = self.downsample(x) if self.downsample else x
+
+        out = self.block1(x)
+        out = self.block2(out, residual)
+
+        return out
+
+    def outputLayer(self):
+        return self.block2
+
+    # input_bitwidth is a list of bitwidth per feature map
+    def getBops(self, input_bitwidth):
+        bops = self.block1.getBops(input_bitwidth)
+        if self.downsample:
+            bops += self.downsample.getBops(input_bitwidth)
+
+        input_bitwidth = self.block1.getCurrentOutputBitwidth()
+        bops += self.block2.getBops(input_bitwidth)
+
+        return bops
+
+    def getCurrentOutputBitwidth(self):
+        return self.block2.getCurrentOutputBitwidth()
+
+    def getOutputBitwidthList(self):
+        return self.block2.getOutputBitwidthList()
+
+    # # select random alpha
+    # def chooseRandomPath(self):
+    #     if self.downsample:
+    #         self.downsample.chooseRandomPath()
+    #
+    #     self.block1.chooseRandomPath()
+    #     self.block2.chooseRandomPath()
+
+    # select alpha based on alphas distribution
+    def choosePathByAlphas(self):
+        if self.downsample:
+            self.downsample.choosePathByAlphas()
+
+        self.block1.choosePathByAlphas()
+        self.block2.choosePathByAlphas()
+
+    def evalMode(self):
+        if self.downsample:
+            self.downsample.evalMode()
+
+        self.block1.evalMode()
+        self.block2.evalMode()
+
+    def numOfOps(self):
+        return self.block2.numOfOps()
+
+
+class ResNet(BaseNet):
+    def __init__(self, args):
+        super(ResNet, self).__init__(args, initLayersParams=(args.bitwidth, args.kernel, args.nClasses))
+
+        # turn on noise in 1st layer
+        if len(self.layersList) > 0:
+            layerIdx = 0
+            self.layersList[layerIdx].turnOnNoise(layerIdx)
+
+        # update model parameters() function
+        self.parameters = self.getLearnableParams
+
+    @staticmethod
+    def createMixedLayer(bitwidths, in_planes, out_planes, kernel_sizes, stride, input_size, prevLayer):
+        f = createMixedConvWithReLU(bitwidths, in_planes, kernel_sizes, stride, input_size, prevLayer)
+        layer = MixedLayer(out_planes, f)
+
+        if layer.numOfOps() > 1:
+            layer.setAlphas([0., 0., 0., 0.25, 0.75])
+            layer.setFiltersPartition()
+
+        return layer
+
+    # init layers (type, in_planes, out_planes)
+    def initLayersPlanes(self):
+        return [(self.createMixedLayer, 3, 64, 32),
+                (BasicBlock, 64, 64, [32]), (BasicBlock, 64, 64, [32]), (BasicBlock, 64, 128, [32, 16]),
+                (BasicBlock, 128, 128, [16]), (BasicBlock, 128, 256, [16, 8]), (BasicBlock, 256, 256, [8]),
+                (BasicBlock, 256, 512, [8, 4]), (BasicBlock, 512, 512, [4])]
+
+    def initLayers(self, params):
+        bitwidths, kernel_sizes, nClasses = params
+        bitwidths = bitwidths.copy()
+
+        layersPlanes = self.initLayersPlanes()
+
+        # init previous layer
+        prevLayer = None
+
+        # create list of layers from layersPlanes
+        # supports bitwidth as list of ints, i.e. same bitwidths to all layers
+        # supports bitwidth as list of lists, i.e. specific bitwidths to each layer
+        layers = ModuleList()
+        for i, (layerType, in_planes, out_planes, input_size) in enumerate(layersPlanes):
+            # build layer
+            l = layerType(bitwidths, in_planes, out_planes, kernel_sizes, 1, input_size, prevLayer)
+            # add layer to layers list
+            layers.append(l)
+            # remove layer specific bitwidths, in case of different bitwidths to layers
+            # if isinstance(bitwidths[0], list):
+            #     nMixedOpLayers = 1 if isinstance(l, MixedFilter) \
+            #         else sum(1 for _, m in l._modules.items() if isinstance(m, MixedFilter))
+            #     del bitwidths[:nMixedOpLayers]
+            # # update previous layer
+            # prevLayer = l.outputLayer()
+
+        self.avgpool = AvgPool2d(4)
+        # self.fc = MixedLinear(bitwidths, 64, 10)
+        self.fc = Linear(512, nClasses).cuda()
+
+        return layers
+
+    def forward(self, x):
+        out = x
+        for layer in self.layers:
+            out = layer(out)
+
+        out = self.avgpool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+
+        return out
+
+    def getLearnableParams(self):
+        return self.learnable_params
+
+    def turnOnWeights(self):
+        for layerIdx, layer in enumerate(self.layersList):
+            assert (layer.added_noise is False)
+            assert (layer.quantized is True)
+            # remove quantization
+            layer.unQuantize(layerIdx)
+            # turn on gradients
+            layer.turnOnGradients(layerIdx)
+
+        # turn on noise in 1st layer
+        if len(self.layersList) > 0:
+            layerIdx = 0
+            layer = self.layersList[layerIdx]
+            layer.turnOnNoise(layerIdx)
+
+        # update learnable parameters
+        self.learnable_params = [param for param in self.parameters() if param.requires_grad]
+        # reset nLayersQuantCompleted
+        self.nLayersQuantCompleted = 0
+
+    def switch_stage(self, loggerFuncs=[]):
+        print('*** switch_stage() ***')
+        # check whether we have to perform a switching stage, or there are no more stages left
+        conditionFlag = self.nLayersQuantCompleted < len(self.layersList)
+        if conditionFlag:
+            layer = self.layersList[self.nLayersQuantCompleted]
+            # assert (layer.alphas.requires_grad is False)
+
+            # turn off noise in layers ops
+            layer.turnOffNoise(self.nLayersQuantCompleted)
+            # quantize layer
+            layer.quantize(self.nLayersQuantCompleted)
+            # turn off gradients
+            layer.quantActOnTraining(self.nLayersQuantCompleted)
+
+            # update learnable parameters
+            self.learnable_params = [param for param in self.parameters() if param.requires_grad]
+
+            # we have completed quantization of one more layer
+            self.nLayersQuantCompleted += 1
+
+            if self.nLayersQuantCompleted < len(self.layersList):
+                layer = self.layersList[self.nLayersQuantCompleted]
+                # turn on noise in the new layer we want to quantize
+                layer.turnOnNoise(self.nLayersQuantCompleted)
+                # for op in layer.getOps():
+                #     assert (op.noise is False)
+                #     op.noise = True
+
+            logMsg = 'nLayersQuantCompleted:[{}/{}], learnable_params:[{}], learnable_alphas:[{}]' \
+                .format(self.nLayersQuantCompleted, self.nLayers(), len(self.learnable_params),
+                        len(self.learnable_alphas))
+
+            # log message to all loggers
+            for f in loggerFuncs:
+                f(logMsg)
+
+        return conditionFlag
+
+    def buildStateDictMap(self, chckpntDict):
+        map = {}
+        map['conv1'] = 'layers.0.ops.0.0.op.0'
+        map['bn1'] = 'layers.0.bn'
+        map['relu'] = 'layers.0.ops.0.0.op.1'
+
+        layersNumberMap = [(1, 0, 1), (1, 1, 2), (2, 1, 4), (3, 1, 6), (4, 1, 8)]
+        for n1, n2, m in layersNumberMap:
+            map['layer{}.{}.conv1'.format(n1, n2)] = 'layers.{}.block1.ops.0.0.op.0'.format(m)
+            map['layer{}.{}.bn1'.format(n1, n2)] = 'layers.{}.block1.bn'.format(m)
+            map['layer{}.{}.relu1'.format(n1, n2)] = 'layers.{}.block1.ops.0.0.op.1'.format(m)
+            map['layer{}.{}.conv2'.format(n1, n2)] = 'layers.{}.block2.ops.0.0.op.0'.format(m)
+            map['layer{}.{}.bn2'.format(n1, n2)] = 'layers.{}.block2.bn'.format(m)
+            map['layer{}.{}.relu2'.format(n1, n2)] = 'layers.{}.block2.ops.0.0.op.1'.format(m)
+
+        downsampleLayersMap = [(2, 0, 3), (3, 0, 5), (4, 0, 7)]
+        for n1, n2, m in downsampleLayersMap:
+            map['layer{}.{}.conv1'.format(n1, n2)] = 'layers.{}.block1.ops.0.0.op.0'.format(m)
+            map['layer{}.{}.bn1'.format(n1, n2)] = 'layers.{}.block1.bn'.format(m)
+            map['layer{}.{}.relu1'.format(n1, n2)] = 'layers.{}.block1.ops.0.0.op.1'.format(m)
+            map['layer{}.{}.conv2'.format(n1, n2)] = 'layers.{}.block2.ops.0.0.op.0'.format(m)
+            map['layer{}.{}.bn2'.format(n1, n2)] = 'layers.{}.block2.bn'.format(m)
+            map['layer{}.{}.relu2'.format(n1, n2)] = 'layers.{}.block2.ops.0.0.op.1'.format(m)
+            map['layer{}.{}.downsample.0'.format(n1, n2)] = 'layers.{}.downsample.ops.0.0.op.0'.format(m)
+            map['layer{}.{}.downsample.1'.format(n1, n2)] = 'layers.{}.downsample.bn'.format(m)
+
+        map['fc'] = 'fc'
+
+        return map
+
+    # load original pre_trained model of UNIQ
+    def loadUNIQPreTrained(self, chckpntDict):
+        map = self.buildStateDictMap(chckpntDict)
+        newStateDict = OrderedDict()
+
+        # check that chckpntDict & map have same keys
+        mapKeys = set(map.keys())
+        chckpntKeys = set([key[:key.rindex('.')] for key in chckpntDict.keys()])
+        dictDiff = mapKeys.symmetric_difference(chckpntKeys)
+        if len(dictDiff) > 0:
+            return False
+
+        token = '.ops.'
+        for key in chckpntDict.keys():
+            # copy non-quantized fc layer weight & bias values
+            if key.startswith('fc.'):
+                if key.endswith('.weight') or key.endswith('.bias'):
+                    newStateDict[key] = chckpntDict[key]
+                continue
+
+            prefix = key[:key.rindex('.')]
+            suffix = key[key.rindex('.'):]
+
+            newKey = map[prefix]
+            # find new key layer
+            idx = newKey.find(token)
+            if idx >= 0:
+                newKeyOp = newKey[:idx]
+                # init path to layer
+                layerPath = [p for p in newKeyOp.split('.')]
+                # get layer by walking through path
+                layer = self
+                for p in layerPath:
+                    layer = getattr(layer, p)
+                # create layer filters dict keys
+                for filterIdx in range(layer.nFilters()):
+                    # create filter state dict key
+                    filterKey = '{}.filters.{}'.format(newKeyOp, filterIdx) + newKey[idx:]
+                    # take the specific filter values from the filters block
+                    filterValues = chckpntDict[key].narrow(0, filterIdx, 1) if len(chckpntDict[key].size()) > 1 else \
+                    chckpntDict[key]
+                    # get the specific filter from layer
+                    filter = layer.filters[filterIdx]
+                    # update filter ops
+                    for j in range(filter.nOpsCopies()):
+                        for i in range(filter.numOfOps()):
+                            newStateKey = filterKey.replace(token + '0.0.', token + '{}.{}.'.format(j, i))
+                            newStateDict[newStateKey + suffix] = filterValues
+
+            elif newKey.endswith('.bn'):
+                # copy batch norm keys
+                newStateDict[newKey + suffix] = chckpntDict[key]
+
+        # load model weights
+        self.load_state_dict(newStateDict)
+
+        # layer_basis is a function of filter quantization,
+        # therefore we have to update its value bases on weight_max_int, which is a function of weights bitwidth
+        for layer in self.layersList:
+            for filter in layer.filters:
+                for j in range(filter.nOpsCopies()):
+                    for i in range(filter.numOfOps()):
+                        op = filter.ops[j][i]
+                        conv = op.op[0]
+                        # update layer_basis value based on weights bitwidth
+                        conv.layer_basis = conv.initial_clamp_value / op.quantize.weight_max_int
+
+    # load weights from the same model but with single operation per layer, like a uniform bitwidth trained model
+    def loadSingleOpPreTrained(self, chckpntDict):
+        newStateDict = OrderedDict()
+
+        token = '.ops.'
+        for key in chckpntDict.keys():
+            if (key.startswith('fc.')) or (token not in key):
+                newStateDict[key] = chckpntDict[key]
+                continue
+
+            prefix = key[:key.rindex('.')]
+            suffix = key[key.rindex('.'):]
+
+            # find new key layer
+            newKeyOp = prefix[:prefix.index(token)]
+            # init path to layer
+            layerPath = [p for p in newKeyOp.split('.')]
+            # get layer by walking through path
+            layer = self
+            for p in layerPath:
+                layer = getattr(layer, p)
+            # update layer ops
+            for j in range(layer.nOpsCopies()):
+                for i in range(layer.numOfOps()):
+                    newKey = prefix.replace(newKeyOp + token + '0.0.', newKeyOp + token + '{}.{}.'.format(j, i))
+                    newStateDict[newKey + suffix] = chckpntDict[key]
+
+        # check that model & newStateDict have same keys
+        newStateKeys = set(newStateDict.keys())
+        dictDiff = newStateKeys.symmetric_difference(set(self.state_dict().keys()))
+        if len(dictDiff) > 0:
+            return False
+
+        # load model weights
+        self.load_state_dict(newStateDict)
--- cnn/models/__init__.py
+++ cnn/models/__init__.py
@@ -1,5 +1,6 @@
 from .BaseNet import BaseNet
 from .TinyNet import TinyNet as tinynet
 from .ResNet import ResNet as resnet
+from .ResNetNice import ResNet as resnet_nice
 from .ThinResNet import ThinResNet as thin_resnet
 from .ModifiedResNet import ModifiedResNet as mod_resnet
