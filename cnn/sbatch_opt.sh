97f9597009faefdb11dfc4f4926ca9bfd9cacba7
brianchmiel <pancho2787>
2018-10-23 22:07:07 +0300

    * updated due to changes in working with remove server

--- cnn/sbatch_opt.sh
+++ cnn/sbatch_opt.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-#SBATCH -c 3 # number of cores
-#SBATCH --gres=gpu:1 # number of gpu requested
 #SBATCH -J "OPT"
 #SBATCH -t 01-00:00:00
 #SBATCH -p all
-#SBATCH -w gaon6
 #SBATCH --mail-user=yochaiz@cs.technion.ac.il
 #SBATCH --mail-type=ALL # Valid values are NONE, BEGIN, END, FAIL, REQUEUE, ALL
 
-source /etc/profile.d/modules.sh
-module load cuda
-cd /home/yochaiz/DropDarts/cnn
-source ~/tf3/bin/activate # activate python3 virtual environment
+# source /etc/profile.d/modules.sh
+# module load cuda
+source ~/venv/bin/activate # activate python3 virtual environment
+cd ~/F-BANNAS/cnn
 echo $1
 echo $2
-PYTHONPATH=/home/yochaiz/DropDarts python3 sbatch_opt.py $1 $2
-
+PYTHONPATH=../ python3 sbatch_opt.py $1 "$2"
