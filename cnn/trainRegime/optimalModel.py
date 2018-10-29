5411247adb19daf496aabd082962b1050a8c8b89
brianchmiel <pancho2787>
2018-10-29 18:28:57 +0200

    * no more calculation of statistics at the moment

--- cnn/trainRegime/optimalModel.py
+++ cnn/trainRegime/optimalModel.py
@@ -17,20 +17,23 @@ class OptimalModel(TrainRegime):
         logger.addInfoTable(title='Bops', rows=[['Total', bopsStr], ['Ratio', bopsRatioStr]])
 
     def train(self):
-        # model = self.model.module
-        model = self.model
-        # make sure model is quantized
-        assert (model.isQuantized() is True)
-        # update statistics in current model, i.e. last checkpoint
-        model.calcStatistics(self.statistics_queue)
-        # save model checkpoint
-        checkpoint, (lastPath, _) = save_checkpoint(self.trainFolderPath, model, self.args, self.epoch,
-                                                    getattr(self.args, self.validAccKey, 0.0), is_best=False)
-        checkpoint['updated_statistics'] = True
-        saveModel(checkpoint, lastPath)
-        self.logger.addInfoToDataTable('Updated statistics in last checkpoint: [{}]'.format(checkpoint['updated_statistics']))
+        pass
 
-        # update statistics in optimal model
-        optCheckpoint, optPath = self.optimalModelCheckpoint
-        statisticsWereUpdated = model.updateCheckpointStatistics(optCheckpoint, optPath, self.statistics_queue)
-        self.logger.addInfoToDataTable('Updated statistics in optimal checkpoint: [{}]'.format(statisticsWereUpdated))
+    # def train(self):
+    #     # model = self.model.module
+    #     model = self.model
+    #     # make sure model is quantized
+    #     assert (model.isQuantized() is True)
+    #     # update statistics in current model, i.e. last checkpoint
+    #     model.calcStatistics(self.statistics_queue)
+    #     # save model checkpoint
+    #     checkpoint, (lastPath, _) = save_checkpoint(self.trainFolderPath, model, self.args, self.epoch,
+    #                                                 getattr(self.args, self.validAccKey, 0.0), is_best=False)
+    #     checkpoint['updated_statistics'] = True
+    #     saveModel(checkpoint, lastPath)
+    #     self.logger.addInfoToDataTable('Updated statistics in last checkpoint: [{}]'.format(checkpoint['updated_statistics']))
+    #
+    #     # update statistics in optimal model
+    #     optCheckpoint, optPath = self.optimalModelCheckpoint
+    #     statisticsWereUpdated = model.updateCheckpointStatistics(optCheckpoint, optPath, self.statistics_queue)
+    #     self.logger.addInfoToDataTable('Updated statistics in optimal checkpoint: [{}]'.format(statisticsWereUpdated))
