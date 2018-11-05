e87469a9fd899e6ab6afcfd4a0ad4744e3e8d802
brianchmiel <pancho2787>
2018-11-05 12:39:26 +0200

    * added prints in removeQuantizationFromStagedLayers() & restoreQuantizationForStagedLayers()

--- cnn/models/BaseNet.py
+++ cnn/models/BaseNet.py
@@ -482,6 +482,7 @@ class BaseNet(Module):
     # we keep ActQaunt.qunatize_during_training == True
     def removeQuantizationFromStagedLayers(self):
         for layerIdx in range(self.nLayersQuantCompleted):
+            print('removed quantization in layer [{}]'.format(layerIdx))
             layer = self.layersList[layerIdx]
             assert (layer.quantized is True)
             # quantize layer ops
@@ -493,6 +494,7 @@ class BaseNet(Module):
     # we keep ActQaunt.qunatize_during_training == True
     def restoreQuantizationForStagedLayers(self):
         for layerIdx in range(self.nLayersQuantCompleted):
+            print('quantized layer [{}]'.format(layerIdx))
             layer = self.layersList[layerIdx]
             assert (layer.quantized is True)
             # quantize layer ops
