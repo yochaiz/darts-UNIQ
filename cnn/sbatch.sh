152a5bb3eab5ee8a84942b24a0f2449cde1c3ed5
brianchmiel <pancho2787>
2018-11-15 11:02:16 +0200

    * adjustments

--- cnn/sbatch.sh
+++ cnn/sbatch.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH -c 3 # number of cores
-#SBATCH --gres=gpu:2 # number of gpu requested
-#SBATCH -J "F-BANNAS"
-#SBATCH -t 01-00:00:00
-#SBATCH -p gip,all
-#SBATCH -w gaon4
+#SBATCH -c 4 # number of cores
+#SBATCH --gres=gpu:1 # number of gpu requested
+#SBATCH -J "Homogeneous-[2,2]"
+# -t 04-00:00:00
+# -p gip,all
+# -w gaon2
 #SBATCH --mail-user=yochaiz@cs.technion.ac.il
 #SBATCH --mail-type=ALL # Valid values are NONE, BEGIN, END, FAIL, REQUEUE, ALL
 
-# source /etc/profile.d/modules.sh
-# module load cuda
+source /etc/profile.d/modules.sh
+module load cuda
 source ~/venv/bin/activate # activate python3 virtual environment
 cd ~/F-BANNAS/cnn
 PYTHONPATH=../ python3 sbatch.py
