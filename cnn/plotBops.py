973e1e1ab88ae207b0fb2754f9e5ac083473f11e
brianchmiel <pancho2787>
2018-11-15 11:02:04 +0200

    * moved code inside a function

--- cnn/plotBops.py
+++ cnn/plotBops.py
@@ -1,8 +1,27 @@
 from cnn.statistics import Statistics
 from torch import load
-from os.path import dirname
+from os.path import dirname, isdir, exists
+from os import listdir
 
-plotPath = '/home/vista/Desktop/Architecture_Search/plots.data.new'
-baseFolder = dirname(plotPath)  # script directory
-plotsData = load(plotPath)
-Statistics.plotBops(plotsData, 'bops', baseFolder)
+
+def plotFromFile(plotPath):
+    baseFolder = dirname(plotPath)  # script directory
+    plotsData = load(plotPath)
+    Statistics.plotBops(plotsData, 'bops', 'Baseline', baseFolder)
+
+
+def plotFromFolder(folderPath, plotsDataPath):
+    plotsData = load(plotsDataPath)
+    for folderName in listdir(folderPath):
+        fPath = '{}/{}'.format(folderPath, folderName)
+        if isdir(fPath):
+            checkpointPath = '{}/train/model_opt.pth.tar'.format(fPath)
+            if exists(checkpointPath):
+                checkpoint = load(checkpointPath)
+
+
+plotPath = '/home/vista/Desktop/Architecture_Search/FF/plots.data'
+# plotFromFile(plotPath)
+
+folderPath = '/home/vista/Desktop/Architecture_Search/FF'
+plotFromFolder(folderPath, plotPath)
