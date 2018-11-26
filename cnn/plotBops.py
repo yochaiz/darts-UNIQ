3b295a4e05d64428b8bc18525394e64e012ae793
brianchmiel <pancho2787>
2018-11-26 16:52:18 +0200

    * separated plot by partition from plot by epochs

--- cnn/plotBops.py
+++ cnn/plotBops.py
@@ -12,17 +12,12 @@ def plotFromFile(plotPath):
     Statistics.plotBops(plotsData, 'bops', 'Baseline', baseFolder)
 
 
-def plotFromFolder(folderPath, plotsDataPath, epoch=None):
+def plotEpochsFromFolder(folderPath, plotsDataPath, epoch=None):
     epochKey = 'epoch'
     bopsKey = 'bops'
     rowKeysToReplace = [TrainRegime.validAccKey, bopsKey]
-    # if epoch is None: # TODO: original
-    #     rowKeysToReplace.append(epochKey)
-
-    # ===== remove ======================
-    counterDict = {}
-    counter = 0
-    # ===================================
+    if epoch is None:
+        rowKeysToReplace.append(epochKey)
 
     plotsData = load(plotsDataPath)
     bopsPlotData = plotsData[bopsKey]
@@ -41,19 +36,54 @@ def plotFromFolder(folderPath, plotsDataPath, epoch=None):
                     existingKeys.append(key)
 
             if len(existingKeys) == len(rowKeysToReplace):
-                # epoch = getattr(checkpoint, epochKey, epoch) #TODO: original
+                epoch = getattr(checkpoint, epochKey, epoch)
+                # add key to bopsPlotData dictionary, if doesn't exist
+                if epoch not in bopsPlotData:
+                    bopsPlotData[epoch] = []
+
+                # add point data
+                title = epoch if isinstance(epoch, str) else None
+                bopsPlotData[epoch].append((title, getattr(checkpoint, bopsKey), getattr(checkpoint, TrainRegime.validAccKey)))
+
+    # save updated plots data
+    save(plotsData, plotsDataPath)
+    # plot
+    Statistics.plotBops(plotsData, bopsKey, 'Baseline', folderPath)
+
+
+def plotPartitionsFromFolder(folderPath, plotsDataPath):
+    bopsKey = 'bops'
+    rowKeysToReplace = [TrainRegime.validAccKey, bopsKey]
+
+    counterDict = {}
+    counter = 0
+
+    plotsData = load(plotsDataPath)
+    bopsPlotData = plotsData[bopsKey]
+    for folderName in sorted(listdir(folderPath)):
+        fPath = '{}/{}'.format(folderPath, folderName)
+        if isfile(fPath):
+            checkpoint = load(fPath)
+            # init list of existing keys we found in checkpoint
+            existingKeys = []
+            # update keys if they exist
+            for key in rowKeysToReplace:
+                v = getattr(checkpoint, key, None)
+                # if v is not None, then update value in tables
+                if v is not None:
+                    # update key exists
+                    existingKeys.append(key)
+
+            if len(existingKeys) == len(rowKeysToReplace):
                 epoch = str(checkpoint.partition[0].tolist())
                 # add key to bopsPlotData dictionary, if doesn't exist
                 if epoch not in counterDict:
-                    # ===== remove ======================
                     counterDict[epoch] = counter
                     counter += 1
                     epochKey = '{}-[{}]'.format(epoch, counterDict[epoch])
                     bopsPlotData[epochKey] = []
-                    # ===================================
 
                 # add point data
-                # title = epoch if isinstance(epoch, str) else None #TODO: original
                 title = '[{}]'.format(counterDict[epoch]) if isinstance(epoch, str) else None
                 epochKey = '{}-[{}]'.format(epoch, counterDict[epoch])
                 bopsPlotData[epochKey].append((title, getattr(checkpoint, bopsKey), getattr(checkpoint, TrainRegime.validAccKey)))
@@ -68,7 +98,7 @@ plotPath = '/home/vista/Desktop/Architecture_Search/FF/plots.data'
 # plotFromFile(plotPath)
 
 folderPath = '/home/vista/Desktop/Architecture_Search/FF'
-plotFromFolder(folderPath, plotPath)
+plotPartitionsFromFolder(folderPath, plotPath)
 
 # # ====== average key values =============
 # from torch import load, save
