0dafd82c6e03db38ecfb6d0e4df38b0cab5c366c
brianchmiel <pancho2787>
2018-11-29 11:17:39 +0200

    * added resnet_flq

--- cnn/models/__init__.py
+++ cnn/models/__init__.py
@@ -1,6 +1,7 @@
 from .BaseNet import BaseNet
 from .TinyNet import TinyNet as tinynet
 from .ResNet import ResNet as resnet
+from .ResNetFLQ import ResNetFLQ as resnet_flq
 from .ResNetNice import ResNet as resnet_nice
 from .ThinResNet import ThinResNet as thin_resnet
 from .ModifiedResNet import ModifiedResNet as mod_resnet
